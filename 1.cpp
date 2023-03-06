e6906083e920278bf3ca86a475363b0186582062
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-03-06 17:52:26 +0700

    first commit

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main ()
+{
+
+    double p,l,luas;
+    cout<<"masukan panjang = ";
+    cin>>p;
+    cout<<"masukan lebar = ";
+    cin>>l;
+    luas=l*p;
+    cout<<"Luasnya adalah = "<<luas;
+}
